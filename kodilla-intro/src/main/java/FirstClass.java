c2742decabab686686e3293430033ccac5fb75d7
Michal Zegarlinski <michzeg@gmail.com>
2021-09-27 12:26:36 +0200

    Helloworld

---
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -0,0 +1,5 @@
+public class FirstClass {                           // [1]
+    public static void main(int[] args) {         // [2]
+        System.out.println(34); // [3]
+    }
+}
---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void  main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Michal");
+        System.out.println("Zegarlinski");
+        System.out.println("Z");
+        System.out.println(34);
+    }
+    public static void main(int[] args) {
+    }
+}
