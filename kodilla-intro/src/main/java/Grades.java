36df12d4bff07b751bc69ca1f5f8b42db8e7923e
Michal Zegarlinski <michzeg@gmail.com>
2021-10-14 13:40:58 +0200

    cwiczenie 2.4 tablice

---
+++ kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,28 @@
+public class Grades {
+    private  int[] grades;
+    private  int size;
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+    public int lastAdded(int value) {
+        return value;
+    }
+    public int averageGrade(int[] grades) {
+        int result = 0;
+        for (int i = 0; i<grades.length; i++) {
+            result = (result + grades[i])/grades.length;
+        }
+        return result;
+    }
+
+
+}
