d31f42552bb3f1542cd0bd866f6892fb791b7b77
Michal Zegarlinski <michzeg@gmail.com>
2021-10-14 11:30:47 +0200

    Zadanie instrukcje warunkowe

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,5 +1,22 @@
-public class FirstClass {                           // [1]
-    public static void main(int[] args) {         // [2]
-        System.out.println(34); // [3]
+public class FirstClass {
+    public static void main(String[] args) {
+        Notebook notebook = new Notebook(600,1000, 2019);
+        System.out.println(notebook.weight + " " + notebook.price);
+        notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.checkYear();
+
+        Notebook heavyNotebook = new Notebook(2000, 1500, 2021);
+        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price);
+        heavyNotebook.checkPrice();
+        heavyNotebook.checkWeight();
+        heavyNotebook.checkYear();
+
+        Notebook oldNotebook = new Notebook(1200, 500, 2016);
+        System.out.println(oldNotebook.weight);
+        System.out.println(oldNotebook.price);
+        oldNotebook.checkPrice();
+        oldNotebook.checkWeight();
+        oldNotebook.checkYear();
     }
 }
