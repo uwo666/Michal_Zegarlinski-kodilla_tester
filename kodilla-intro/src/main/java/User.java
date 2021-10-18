e99eef94de22952c92b3e48ee2f86d9858c8edca
Michal Zegarlinski <Michzeg@gmail.com>
2021-10-18 23:49:04 +0200

    2,5

--- kodilla-intro/src/main/java/User.java
+++ kodilla-intro/src/main/java/User.java
@@ -1,11 +1,40 @@
 public class User {
     String name;
     int age;
-        public User(int age, String name) {
+        public User(String name, int age) {
             this.name = name;
             this.age = age;
         }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public static void main(String[] args){
+        User anna = new User("Anna", 20);
+        User betty = new User("Betty", 33);
+        User carl = new User("Carl", 58);
+        User david = new User("David", 13);
+        User eva = new User("Eva", 18);
+        User frankie = new User("Frankie", 45);
+        User[] users = {anna, betty, carl, david, eva, frankie};
+
+    }
+    public int averageAge(int[] users) {
+        int result = 0;
+        for (int i = 0; i<users.length; i++) {
+            result = (result + age)/users.length;
+        }
+        return result;
 
 
+    }
+    public int usersUnderAverageAge(int[] users) {
+            for (int i = 0; i<; )
+
+    }
 }
