0ebeb10e6396f6b68ea7d6b71f62e5a05450ce16
Michal Zegarlinski <michzeg@gmail.com>
2021-10-19 11:26:56 +0200

    Zadanie 2.6 refaktoryzacja

---
+++ kodilla-intro/src/main/java/Application.java
@@ -0,0 +1,25 @@
+public class Application {
+    String name;
+    double age;
+    double height;
+
+    public Application(String name, double age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+    public void checkCharacteristics(){
+        if (name != null) {
+            if (age > 30 && height > 160) {
+                System.out.println("User is older than 30 and higher than 160cm");
+            } else {
+                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Application application = new Application("Adam", 40.5, 178);
+        application.checkCharacteristics();
+    }
+}
+
