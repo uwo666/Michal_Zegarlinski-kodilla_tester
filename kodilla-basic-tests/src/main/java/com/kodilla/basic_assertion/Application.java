1ad1f6e1f6aa57775ca5ccd8bbade1ae82c8f335
Michal Zegarlinski <Michzeg@gmail.com>
2021-10-25 21:31:52 +0200

    Zadanie 3,5 gotowe

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -0,0 +1,33 @@
+package com.kodilla.basic_assertion;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a, b);
+        int subtResult = calculator.subtract(a, b);
+        int squareResult = calculator.square(a);
+        boolean correct = ResultChecker.assertEquals(13, sumResult);
+        if (correct) {
+            System.out.println("Metoda sum działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda sum nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+
+        boolean correctSub = ResultChecker.assertEquals(-3, subtResult);
+        if (correctSub) {
+            System.out.println("Metoda subtract działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda subtract nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+
+        boolean correctSquare = ResultChecker.assertEquals(25, squareResult );
+        if (correctSquare){
+            System.out.println("Metoda square działa poprawnie dla liczby " + a );
+        }else {
+            System.out.println("Metoda square nie działa poprawnie dla liczb " + a);
+        }
+    }
+}
