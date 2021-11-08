fac3832e9ef14930b3736b64e0943bad33f7515e
Michal Zegarlinski <michzeg@gmail.com>
2021-11-08 13:31:43 +0100

    poprawiona mutowalność

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/Book.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/Book.java
@@ -1,19 +1,19 @@
 package com.kodilla.collections.adv.immutable;
 
-public final class  Book  {
-    protected final String author;
-    protected final String title;
+public  class  Book  {
+    protected  String author;
+    protected  String title;
 
     public Book(String author, String title) {
         this.author = author;
         this.title = title;
     }
 
-    public final String getAuthor() {
+    public  String getAuthor() {
         return author;
     }
 
-    public final String getTitle() {
+    public  String getTitle() {
         return title;
     }
 }
