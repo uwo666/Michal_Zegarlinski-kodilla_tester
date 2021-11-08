110c207312824971dcb297a66fc7856b970b13e1
Michal Zegarlinski <michzeg@gmail.com>
2021-11-08 12:44:04 +0100

    mutowalność

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/BookHacked.java
@@ -0,0 +1,22 @@
+package com.kodilla.collections.adv.immutable;
+
+//public class BookHacked extends Book{
+//    private String realTitle;
+//
+//    public BookHacked(String author, String title) {
+//    super(author, title);
+//    realTitle = title; // obejscie final
+//}
+//
+//    public void modifyTitle(String newTitle) { // obejście protected
+//        title = newTitle;}
+//
+//    public void modifyTitle(String newTitle) {
+//        realTitle = newTitle;
+//    }
+//
+//    @Override                     // obejście final bez final gettera
+//    public String getTitle() {
+//        return realTitle;
+//    }
+//}
