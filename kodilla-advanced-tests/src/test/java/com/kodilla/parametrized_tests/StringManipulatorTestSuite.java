ecf8146ac7edc510c623514d70a1671eb7655fca
Michal Zegarlinski <michzeg@gmail.com>
2021-11-18 15:46:57 +0100

    zadanie 7.1

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
@@ -1,6 +1,7 @@
 package com.kodilla.parametrized_tests;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,5 +25,13 @@ public class StringManipulatorTestSuite {
     @CsvSource(value = {"te,st:1", "..OtHEr :0", "E,V,e,n.t:3", "null :0", "A:0"}, delimiter = ':')
     public void shouldCountNumberOfCommas(String input, int expected) {
         assertEquals(expected, manipulator.countNumberOfCommas(input));
+    }
+
+    // 7.2
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/stringWithLowerCase.csv", numLinesToSkip = 1)
+    public void shouldReverseStringWithLowerCase2(String input, String expected) {
+        assertEquals(expected, manipulator.reverseWithLowerCase(input));
     }
 }
