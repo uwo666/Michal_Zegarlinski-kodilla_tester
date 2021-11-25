a2d29620dd5f0b4c87f63699aac216a5cdac8ca9
Michal Zegarlinski <michzeg@gmail.com>
2021-11-25 08:52:16 +0100

    7 koniec

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonSource.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonSource.java
@@ -7,17 +7,17 @@ import java.util.stream.Stream;
 public class PersonSource {
     static Stream<Arguments> providePersonForTestingBmi() {
         return Stream.of(
-                Arguments.of("Very severely underweight",1,15),
-                Arguments.of("Severely underweight",15, 16),
-                Arguments.of("Underweight", 16,18.5),
-                Arguments.of("Normal (healthy weight)", 18.5,25),
-                Arguments.of("Overweight", 25,30),
-                Arguments.of("Obese Class I (Moderately obese)",30,35),
-                Arguments.of("Obese Class II (Severely obese)", 35,40),
-                Arguments.of("Obese Class III (Very severely obese)",40,45),
-                Arguments.of("Obese Class IV (Morbidly Obese)", 45,50),
-                Arguments.of("Obese Class V (Super Obese)", 50,60),
-                Arguments.of("Obese Class VI (Hyper Obese)", 61,300)
+                Arguments.of("Very severely underweight",1.80,40),
+                Arguments.of("Severely underweight",1.8, 51),
+                Arguments.of("Underweight", 1.8,55),
+                Arguments.of("Normal (healthy weight)", 1.8,60),
+                Arguments.of("Overweight", 1.6,75),
+                Arguments.of("Obese Class I (Moderately obese)",1.5,70),
+                Arguments.of("Obese Class II (Severely obese)", 1.4,70),
+                Arguments.of("Obese Class III (Very severely obese)",1.7,120),
+                Arguments.of("Obese Class IV (Morbidly Obese)", 1.7,140),
+                Arguments.of("Obese Class V (Super Obese)", 1.6,140),
+                Arguments.of("Obese Class VI (Hyper Obese)", 1.6,160)
 
         );
     }// przerób liczby naw\zrost i wagę
--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
@@ -7,15 +7,14 @@ import org.junit.jupiter.params.provider.MethodSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTestSuite {
-    Person person = new Person(162,44);
+
 
         @ParameterizedTest
         @MethodSource(value = "com.kodilla.parametrized_tests.homework.PersonSource#providePersonForTestingBmi")
-        public void shouldCalculateBmi(String input, int height, int weight) {
+        public void shouldCalculateBmi(String input, double height, double weight) {
             Person person = new Person(height, weight);
             assertEquals(input, person.getBMI());
 
         }
 }
 
-// assertEquals(expected, manipulator.getStringLengthWithoutSpaces(input));
