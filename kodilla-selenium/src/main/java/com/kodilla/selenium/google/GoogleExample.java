301ed110f03051e7b7a2153069c5355c15c9c875
Michal Zegarlinski <michzeg@gmail.com>
2022-01-07 15:14:34 +0100

    13,3

--- kodilla-selenium/src/main/java/com/kodilla/selenium/google/GoogleExample.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/google/GoogleExample.java
@@ -4,16 +4,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GoogleExample {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
+        driver.navigate().to("http://www.google.com");
+        driver.switchTo().frame(0);
+        WebElement inputField1 = driver.findElement(By.id("L2AGLb"));
+        inputField1.submit();
 
         WebElement inputField = driver.findElement(By.name("q"));
         inputField.sendKeys("Kodilla");
         inputField.submit();
+        WebDriverWait wait = new WebDriverWait(driver,20);
 
     }
 }
