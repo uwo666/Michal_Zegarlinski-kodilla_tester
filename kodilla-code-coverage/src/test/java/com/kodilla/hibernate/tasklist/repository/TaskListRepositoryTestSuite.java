d8dcfc340a6148599771b665ed0f4fc11d99c3b7
Michal Zegarlinski <Michzeg@gmail.com>
2022-01-25 22:20:09 +0100

    15.3

---
+++ kodilla-code-coverage/src/test/java/com/kodilla/hibernate/tasklist/repository/TaskListRepositoryTestSuite.java
@@ -0,0 +1,33 @@
+package com.kodilla.hibernate.tasklist.repository;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class TaskListRepositoryTestSuite {
+    @Autowired
+    private TaskListRepository taskListRepository;
+
+    private static final String DESCRIPTION = "Test: Hibernate homework";
+
+    @Test
+    void testFindByListName() {
+        //Given
+        TaskList list = new TaskList("To_Do", DESCRIPTION);
+        taskListRepository.save(list);
+        String name = list.getListName();
+
+        //When
+        List<TaskList> myLists = taskListRepository.findByListName(name);
+
+        //Then
+        Assertions.assertEquals(1, myLists.size());
+
+
+    }
+}
